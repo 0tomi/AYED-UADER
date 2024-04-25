e274bd14b4307c17bd21985426fddfb561d1bac0
0tomi <tomsschlotahuer@hotmail.com>
2024-04-25 01:13:45 -0300

    ds
    d

--- lista/listaDE.cpp
+++ lista/listaDE.cpp
@@ -15,6 +15,34 @@ struct nodeDE{
     nodeDE* back = NULL;
 };
 
+/* test
+void sort2(nodeDE* &list, int data){
+        // version 2 usando 2 punteros
+        if (list == NULL){
+            list = new nodeDE;
+            list->data = data;
+        } else {
+            nodeDE* aux = list;
+            nodeDE* auxanterior = list;
+            nodeDE* newNode = new nodeDE;
+            while((aux != NULL) || (aux->data < data)){
+                if (auxanterior->next == NULL)
+                    aux = auxanterior->next; 
+                auxanterior = aux->next;
+            }
+            newNode->data = data;
+            newNode->next = aux;
+            newNode->back = auxanterior->back;
+            if (newNode->back == NULL){
+                list = newNode;
+                auxanterior->back = list;
+            } else {
+                auxanterior->back->next = newNode;
+                auxanterior->back = newNode;
+            }
+        }
+    }
+*/
 void sorted_insert(nodeDE* &list, int data){
     if (list == NULL){
         list = new nodeDE;
@@ -31,7 +59,10 @@ void sorted_insert(nodeDE* &list, int data){
             newNode->next = aux;
             newNode->back = aux->back;
             // linking new node between the old ones
-            aux->back->next = newNode;
+            if (aux->back == NULL)
+                list = newNode; //placing the node at the first place
+            else
+                aux->back->next = newNode;
             aux->back = newNode;
         } else {
             // if the node has to be at the end of the list:
@@ -53,6 +84,7 @@ int main(int argc, const char** argv) {
     for (int i = 1; i < 5; i++)
         sorted_insert(list, i*2);
 
+    sorted_insert(list,1);
     sorted_insert(list,7);
     sorted_insert(list,5);
     sorted_insert(list,3);
