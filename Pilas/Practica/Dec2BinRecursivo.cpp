d33da43c33dd1c1ee77104dadc725ccc40206acb
0tomi <tomsschlotahuer@hotmail.com>
2024-06-11 21:53:56 -0300

    Update Dec2BinRecursivo.cpp

---
+++ Pilas/Practica/Dec2BinRecursivo.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+// Ejercicio de Dec2Bin en su forma recursiva
+
+/* string Dec2Bin(int number){
+    if (number == 1)
+        return "1";
+    if (number == 0)
+        return "0";
+    return Dec2Bin(number/2) + char('0' + number%2);
+} */
+
+string Dec2Bin(int number){
+    if (number > 1)
+        return Dec2Bin(number/2) + char('0' + number%2);
+    return (number == 0) ? "0": "1";
+}
+
+int main(int argc, char const *argv[])
+{
+    cout << Dec2Bin(5);
+    return 0;
+}
