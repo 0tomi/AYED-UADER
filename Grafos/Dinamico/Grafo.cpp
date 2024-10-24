b88a80491538a95f74edc80e08cae2e54e6851e2
0tomi <tomsschlotahuer@hotmail.com>
2024-10-24 02:18:25 -0300

    Update Grafo.cpp

--- Grafos/Dinamico/Grafo.cpp
+++ Grafos/Dinamico/Grafo.cpp
@@ -12,7 +12,7 @@ private:
     Nodo * nodoOrigen;
 public:
     Arco(int id, Nodo* origen, Nodo* destino);
-    int getId() { return id_arco; }
+    int id() { return id_arco; }
     Nodo* getNodoOrigen() { return nodoOrigen; }
     Nodo* getNodoDestino() { return nodoDestino; }
     ~Arco();
@@ -39,10 +39,12 @@ private:
     ListaDE <Arco> arcos;
 public:
     Nodo(int id_nodo);
+    int id() { return this->id_nodo; }
     void addArco(Arco arc);
     int getCantidadArcos();
-    int checkIfRelacionadoCon(Nodo*);   // 0: No tiene relaciones, mayor a 0, cantidad de relaciones.
+    int checkIfRelacionadoCon(int id_Nodo);   // 0: No tiene relaciones, mayor a 0, cantidad de relaciones.
     bool checkID_ArcoUsada(int id);
+    ListaDE<int> getRight();
     ~Nodo();
 };
 
@@ -51,22 +53,35 @@ Nodo::Nodo(int id_): id_nodo(id_)
 
 void Nodo::addArco(Arco arc)
 {
-    if (arc.getNodoDestino()->id_nodo == this->id_nodo)
+    if (arc.getNodoDestino()->id() == this->id_nodo)
         this->arcos.push_front(arc);
     else this->arcos.push_back(arc);
 }
 
 int Nodo::getCantidadArcos()
 {
-    
+    return this->arcos.getSize();
 }
 
-int Nodo::checkIfRelacionadoCon(Nodo*)
+int Nodo::checkIfRelacionadoCon(int id_Nodo)
 {
-    
+    auto tamanio = this->arcos.getSize();
+    int founded = 0;
+    for (int i = 0; i < tamanio; i++)
+        if (arcos[i].getNodoDestino()->id() == id_Nodo)
+            founded++;
+
+    return founded;
 }
 
-bool Nodo::check
+bool Nodo::checkID_ArcoUsada(int id)
+{
+    auto tamanio = arcos.getSize();
+    for (int i = 0; i < tamanio; i++)
+        if (arcos[i].id() == id)
+            return true;
+    return false;
+}
 
 Nodo::~Nodo()
 {
