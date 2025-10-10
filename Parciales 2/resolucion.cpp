0fb68b0a5a27dccf487d1ade5561a730bec03fd1
tomi <tomsschlotahuer@hotmail.com>
2025-10-10 15:54:08 -0300

    Update resolucion.cpp

--- Parciales 2/resolucion.cpp
+++ Parciales 2/resolucion.cpp
@@ -8,7 +8,45 @@ struct conexion;
 struct router {
     int codigo;
     string pais;
+    int cantidadConexiones;
+    string marca;
+    string fechaCompra;
 
+    conexion * conexiones = nullptr;
+    router * next = nullptr;
 };
 
-struct arco
+struct conexion {
+    string codigo;
+    string tipo;
+    int velocidad;
+    string state;
+
+    router * destino;
+    conexion * next = nullptr;
+};
+
+int contarEstadosArco(conexion * inicio, string state){
+    int cantidadEstados = 0;
+    while (inicio){
+        if (inicio->state == state){
+            cantidadEstados++;
+            cout << "El enlace " << inicio->codigo 
+            << " que llega a " << inicio->destino->codigo 
+            << " esta caido.";
+        }
+        inicio = inicio->next;
+    }
+    return cantidadEstados;
+}
+
+int contarEstados (router * inicio, string state = "Down"){
+    int cantidadEstados = 0;
+    
+    while (inicio){
+        cantidadEstados += contarEstadosArco(inicio->conexiones, state);
+        inicio = inicio->next;
+    }
+    
+    return cantidadEstados;
+}
